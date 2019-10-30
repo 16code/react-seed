4473af6016a49c44e53637f0d4cc7b7b329f9813
16code <130866602@qq.com>
2019-10-30 11:10:00 +0800

    backup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux';
-import { ajaxReducer } from './ajax';
 import { updatedReducer } from './playerState';
 import { lyricBoxVisibleReducer } from './lyricBox';
-import { documentTitleReducer } from './title';
 
 export default combineReducers({
-    ajax: ajaxReducer,
     player: updatedReducer,
-    lyricBox: lyricBoxVisibleReducer,
-    documentTitle: documentTitleReducer
+    lyricBox: lyricBoxVisibleReducer
 });
